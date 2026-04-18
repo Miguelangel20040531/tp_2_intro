5b634d3347317244f25eb69f9576daba8210a678
Federico <programacion.fede@gmail.com>
2026-04-18 04:30:50 -0300

    Añado archivo con validaciones para errores 400

---
+++ validaciones.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+
+def tiene_formato_string(cadena):
+    """ Valido que sea string y que no sea string vacio"""
+    return isinstance(cadena, str) and cadena.strip() != ""
+
+def es_entero(goles):
+    try:
+        int(goles)
+        return True
+    except ValueError:
+        return False
+
+def es_fecha_correcta(fecha):
+    try:
+        datetime.strptime(fecha, "%Y-%m-%d")
+        return True
+    except ValueError:
+        return False

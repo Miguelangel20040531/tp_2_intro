f81841984eb25d95ddc90c05847a8116d22df364
RobertoMaximo <rquisped@fi.uba.ar>
2026-04-18 21:35:04 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3,22 +3,11 @@ from db import get_connection
 
 app = Flask(__name__)
 
+app.register_blueprint(partidos_db, url_prefix="/partidos")
+
 @app.route("/")
 def home():
     return " Mundial 2026 "
-
-@app.route("/partidos")
-def obtener_partidos():
-    conn = get_connection()
-    cursor = conn.cursor()
-
-    cursor.execute("SELECT * FROM fixture")
-
-    partidos = cursor.fetchall()
-
-    conn.close()
-
-    return partidos
 
 if __name__ == "__main__":
     app.run(debug=True)

89d3cab6d45e58e169df9df6e7e8d9dfa1e36e70
RobertoMaximo <rquisped@fi.uba.ar>
2026-04-26 11:44:36 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,17 @@
 from flask import Flask
 from partidos import partidos_db
 from usuarios import usuarios_db
-from db import get_connection
+from ranking import ranking_bp
 
-app = Flask(__name__)
+app= Flask(__name__)
 
 app.register_blueprint(partidos_db, url_prefix="/partidos")
 app.register_blueprint(usuarios_db, url_prefix="/usuarios")
+app.register_blueprint(ranking_bp, url_prefix='/ranking')
 
-@app.route("/")
-def home():
-    return " Mundial 2026 "
 
-if __name__ == "__main__":
-    app.run(debug=True)
+
+
+
+if __name__== "__main__":
+    app.run(port=5000,debug=True)
